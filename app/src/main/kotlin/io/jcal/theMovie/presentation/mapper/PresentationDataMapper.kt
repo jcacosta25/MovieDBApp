59f19771c93d2971e47383f816802546bffb42c9
jcacosta <juank2acosta@gmail.com>
2019-02-14 19:09:27 -0600

    [movie-db-app]<ui>: data mapper integration

--- app/src/main/kotlin/io/jcal/theMovie/presentation/mapper/PresentationDataMapper.kt
+++ app/src/main/kotlin/io/jcal/theMovie/presentation/mapper/PresentationDataMapper.kt
@@ -1,5 +1,151 @@
 package io.jcal.theMovie.presentation.mapper
 
+import io.jcal.movies_provider.repository.mapper.model.DatesModel
+import io.jcal.movies_provider.repository.mapper.model.EpisodeModel
+import io.jcal.movies_provider.repository.mapper.model.MovieModel
+import io.jcal.movies_provider.repository.mapper.model.MoviesModel
+import io.jcal.movies_provider.repository.mapper.model.SeasonModel
+import io.jcal.movies_provider.repository.mapper.model.TvShowModel
+import io.jcal.movies_provider.repository.mapper.model.TvShowsModel
+import io.jcal.theMovie.presentation.mapper.model.DatesUIModel
+import io.jcal.theMovie.presentation.mapper.model.EpisodeUIModel
+import io.jcal.theMovie.presentation.mapper.model.MovieUIModel
+import io.jcal.theMovie.presentation.mapper.model.MovieUIModelList
+import io.jcal.theMovie.presentation.mapper.model.SeasonUIModel
+import io.jcal.theMovie.presentation.mapper.model.TvShowUIList
+import io.jcal.theMovie.presentation.mapper.model.TvShowUIModel
 import javax.inject.Inject
 
-class PresentationDataMapper @Inject constructor()
+class PresentationDataMapper @Inject constructor() {
+
+    fun convert(model: MovieModel): MovieUIModel {
+        val uiModel = MovieUIModel(
+            id = model.id,
+            adult = model.adult,
+            backdropPath = model.backdropPath,
+            budget = model.budget,
+            genreIds = model.genreIds,
+            homepage = model.homepage,
+            imdbId = model.imdbId,
+            originalLanguage = model.originalLanguage,
+            originalTitle = model.originalTitle,
+            overview = model.overview,
+            popularity = model.popularity,
+            posterPath = model.posterPath,
+            releaseDate = model.releaseDate,
+            revenue = model.revenue,
+            runtime = model.runtime,
+            spokenLanguageModels = model.spokenLanguageModels,
+            status = model.status,
+            tagline = model.tagline,
+            title = model.title,
+            video = model.video,
+            voteAverage = model.voteAverage,
+            voteCount = model.voteCount
+        )
+        uiModel.state = model.state
+        return uiModel
+    }
+
+    fun convert(model: DatesModel): DatesUIModel {
+        val uiModel = DatesUIModel(
+            maximum = model.maximum,
+            minimum = model.minimum
+        )
+        uiModel.state = model.state
+        return uiModel
+    }
+
+    fun convert(model: MoviesModel): MovieUIModelList {
+        val uiModel = MovieUIModelList(
+            dates = convert(model.dates),
+            page = model.page,
+            results = model.results.map { convert(it) },
+            totalPages = model.totalPages,
+            totalResult = model.totalResult
+        )
+        uiModel.state = model.state
+        return uiModel
+    }
+
+    fun convert(model: EpisodeModel): EpisodeUIModel {
+        val uiModel = EpisodeUIModel(
+            id = model.id,
+            airDate = model.airDate,
+            episodeNumber = model.episodeNumber,
+            name = model.name,
+            overview = model.overview,
+            productionCode = model.productionCode,
+            seasonNumber = model.seasonNumber,
+            showId = model.showId,
+            stillPath = model.stillPath,
+            voteAverage = model.voteAverage,
+            voteCount = model.voteCount
+        )
+        uiModel.state = model.state
+        return uiModel
+    }
+
+    fun convert(model: SeasonModel): SeasonUIModel {
+        val uiModel = SeasonUIModel(
+            id = model.id,
+            airDate = model.airDate,
+            episodeCount = model.episodeCount,
+            name = model.name,
+            overview = model.overview,
+            posterPath = model.posterPath,
+            seasonNumber = model.seasonNumber,
+            episodes = model.episodes.map { convert(it) },
+            showId = model.showId
+        )
+        uiModel.state = model.state
+        return uiModel
+    }
+
+    fun convert(model: TvShowModel): TvShowUIModel {
+        val uiModel = TvShowUIModel(
+            id = model.id,
+            airDate = model.airDate,
+            episodeCount = model.episodeCount,
+            seasonNumber = model.seasonNumber,
+            backdropPath = model.backdropPath,
+            episodeRunTime = model.episodeRunTime,
+            firstAirDate = model.firstAirDate,
+            homepage = model.homepage,
+            inProduction = model.inProduction,
+            languages = model.languages,
+            lastAirDate = model.lastAirDate,
+            lastEpisodeToAir = model.lastEpisodeToAir,
+            name = model.name,
+            nextEpisodeToAir = model.nextEpisodeToAir,
+            numberOfEpisodes = model.numberOfEpisodes,
+            numberOfSeasons = model.numberOfSeasons,
+            originCountry = model.originCountry,
+            originalLanguage = model.originalLanguage,
+            originalName = model.originalName,
+            overview = model.overview,
+            popularity = model.popularity,
+            posterPath = model.posterPath,
+            seasons = model.seasons.map { convert(it) },
+            status = model.status,
+            type = model.type,
+            voteAverage = model.voteAverage,
+            voteCount = model.voteCount,
+            genreIds = model.genreIds
+        )
+        uiModel.state = model.state
+        return uiModel
+    }
+
+    fun convert(model: TvShowsModel): TvShowUIList {
+        val uiModel = TvShowUIList(
+            dates = convert(model.dates),
+            page = model.page,
+            results = model.results.map { convert(it) },
+            totalPages = model.totalPages,
+            totalResult = model.totalResult
+        )
+        uiModel.state = model.state
+        return uiModel
+    }
+}
